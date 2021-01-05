a73b991866becd5128e24f3d24e575f84861e4b4
Jakov <jakovpfccitler@gmail.com>
2021-01-05 22:39:18 +0100

    second commit

--- force-app/main/default/lwc/newsComponent/newsComponent.js
+++ force-app/main/default/lwc/newsComponent/newsComponent.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import retriveNews from "@salesforce/apex/newsController.retriveNews";
 
-export default class NewsComponent extends LightningElement {}
+export default class NewsComponent extends LightningElement {
+
+    connectedCallback(){
+        this.fetchNews();
+    }
+
+    fetchNews(){
+        retriveNews().then(response => {
+            console.log(response);
+        }).catch(error=>{
+            console.error(error);
+        })
+    }
+
+}
